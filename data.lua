1ca6f0e2aa6fb6762a14bb066c86f23fc1071ca9
Henry Post <HenryFBP@gmail.com>
2017-10-09 14:06:20 -0500

    added extra keybinds for debug purposes

--- data.lua
+++ data.lua
@@ -3,11 +3,23 @@
 data:extend(
   {
   	-- keybindings
-  	{--register a keybind.
+    {--register a keybind.
   		type = "custom-input",
   		name = "export_data_button",
   		key_sequence = "CONTROL + SHIFT + e"
-  	}
-    
+  	},
+
+    {
+      type = "custom-input",
+      name = "export_data_button_2",
+      key_sequence = "CONTROL + SHIFT + s"
+    },
+
+    {
+      type = "custom-input",
+      name = "export_data_button_3",
+      key_sequence = "CONTROL + SHIFT + a"
+    }
+
   }
 )
