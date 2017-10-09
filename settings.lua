e145420a303615f5c167496ca2377813bd646ad2
Henry Post <HenryFBP@gmail.com>
2017-10-08 19:43:49 -0500

    added setting to have an example if i need any settings later

--- settings.lua
+++ settings.lua
@@ -1,11 +1,13 @@
 --settings.lua
 
 data:extend(
-  {{
+  {
+    {
       type = "bool-setting",
       name = "my-mod-test-setting",
       localised_description = "A test setting.",
       setting_type = "runtime-global",
       default_value = true
-  }}
+    }
+  }
 )
