d44f6d304223565b199de8e8c6dea41a5c318b36
Henry Post <HenryFBP@gmail.com>
2017-10-08 19:41:51 -0500

    added keybind for export menu

--- settings.lua
+++ settings.lua
@@ -1,1 +1,11 @@
 --settings.lua
+
+data:extend(
+  {{
+      type = "bool-setting",
+      name = "my-mod-test-setting",
+      localised_description = "A test setting.",
+      setting_type = "runtime-global",
+      default_value = true
+  }}
+)
