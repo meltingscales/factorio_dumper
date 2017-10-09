58a1ff5cf2f9926bee61048dc5382c2defda872e
Henry Post <HenryFBP@gmail.com>
2017-10-08 19:40:07 -0500

    added batch files for easy testing

---
+++ run_factorio_custom_moddir.bat
@@ -0,0 +1,1 @@
+E:\Games\Steam\steamapps\common\Factorio\bin\x64\factorio.exe --mod-directory "C:\Users\henryfbp\Documents\GitHub"
---
+++ separate_console.bat
@@ -0,0 +1,1 @@
+start cmd.exe @cmd /k "run_factorio_custom_moddir"
