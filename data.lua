884d6e09a502e60b95092e5f1fc3458772642a64
Henry Post <HenryFBP@gmail.com>
2017-10-08 19:43:31 -0500

    added keybind for export menu

--- data.lua
+++ data.lua
@@ -1,1 +1,13 @@
 --data.lua
+
+data:extend(
+  {
+  	-- keybindings
+  	{--register a keybind.
+  		type = "custom-input",
+  		name = "export_data_button",
+  		key_sequence = "CONTROL + SHIFT + e"
+  	}
+    
+  }
+)
