233e3fefea813cd3edf8aabb5c9ed7bf2c82fb97
HenryFBP <HenryFBP@gmail.com>
2017-10-16 18:11:08 -0500

    added message to let steam-using contribs develop with stdout

--- run_factorio_custom_moddir.bat
+++ run_factorio_custom_moddir.bat
@@ -1,5 +1,17 @@
+
 E:\Games\Steam\steamapps\common\Factorio\bin\x64\factorio.exe --mod-directory "C:\Users\henryfbp\Documents\GitHub"
 
-echo "seeya!"
+@ECHO OFF
+
+
+echo seeya!
+echo btw, if this is closing with no output, either:
+echo
+echo use a non-steam copy of factorio,
+echo !OR!"
+echo go to Steam\steam.exe, right-click and select properties,
+echo and click the 'run as admin' box.
+echo now every time you are prompted to allow, click deny.
+
 pause
 exit
