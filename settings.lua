6fb57d3cfdc2b91a7d8208798c4ae87224dc9284
HenryFBP <HenryFBP@gmail.com>
2017-10-16 18:11:43 -0500

    settings displays correctly

--- settings.lua
+++ settings.lua
@@ -5,10 +5,12 @@ data:extend(
     {
       type = "bool-setting",
       name = "debug-messages",
-      localised_name = "debug-messages",
-      localised_description = "[facDump] Should we turn on debug messages?",
+      order = "aa",
       setting_type = "runtime-global",
-      default_value = false
+      localised_name = "Debug messages?",
+      localised_description = "[facDump] Should we turn on debug messages?",
+      default_value = false,
+      per_user = true
     }
   }
 )
