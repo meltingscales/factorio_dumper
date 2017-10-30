14df7877f369081ca090ef7e030e87a943a7ba1b
HenryFBP <HenryFBP@gmail.com>
2017-10-30 16:05:29 -0500

    added message for confused cloners/pullers

--- run_factorio_custom_moddir.bat
+++ run_factorio_custom_moddir.bat
@@ -13,6 +13,15 @@ IF EXIST %path2% (
 )
 
 
+
 echo "seeya!"
+echo "btw, if this is closing with no output, either:"
+echo
+echo "use a non-steam copy of factorio,"
+echo "!OR!"
+echo "go to Steam\steam.exe, right-click and select properties,"
+echo "and click the 'run as admin' box."
+echo "now every time you are prompted to allow, click deny."
+
 pause
 exit
