ab5f079ede31e763d2c4ff52e3ec14e11255d339
HenryFBP <HenryFBP@gmail.com>
2017-10-30 16:02:53 -0500

    added logic to unify laptop+desktop factorio-running for testing ease

--- run_factorio_custom_moddir.bat
+++ run_factorio_custom_moddir.bat
@@ -1,4 +1,17 @@
-E:\Games\Steam\steamapps\common\Factorio\bin\x64\factorio.exe --mod-directory "C:\Users\henryfbp\Documents\GitHub"
+@ECHO OFF
+
+SET path1="E:\Games\Steam\steamapps\common\Factorio\bin\x64\factorio.exe"
+SET path2="C:\Program Files (x86)\Steam\steamapps\common\Factorio\bin\x64\factorio.exe"
+SET moddir="C:\Users\henryfbp\Documents\GitHub"
+
+
+IF EXIST %path1% (
+  %path1% --mod-directory %moddir%.
+)
+IF EXIST %path2% (
+  %path2% --mod-directory %moddir%.
+)
+
 
 echo "seeya!"
 pause
